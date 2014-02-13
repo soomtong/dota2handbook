fdb0326f8f97c15a3be7db3f4de09075b34193a4
Soomtong-Mac <soomtong@gmail.com>
2014-02-13 23:14:51 +0900

    escape disabling

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 // 'dota2handbook.services' is found in services.js
 // 'dota2handbook.controllers' is found in controllers.js
 angular.module('dota2handbook', ['ionic', 'dota2handbook.services', 'dota2handbook.controllers'])
-    .config(function($stateProvider, $urlRouterProvider, $httpProvider) {
+    .config(function($stateProvider, $urlRouterProvider, $httpProvider, $sceProvider) {
         $stateProvider
             .state('hero', {
                 url: "/hero",
@@ -12,7 +12,7 @@ angular.module('dota2handbook', ['ionic', 'dota2handbook.services', 'dota2handbo
             })
             .state('heroDetail', {
                 url: "/hero/:heroId",
-                templateUrl: "templates/hero.html",
+                templateUrl: "templates/hero_view.html",
                 controller: 'HeroDetailCtrl'
             })
             .state('item', {
@@ -22,17 +22,33 @@ angular.module('dota2handbook', ['ionic', 'dota2handbook.services', 'dota2handbo
             })
             .state('itemDetail', {
                 url: "/item/:itemId",
-                templateUrl: "templates/item.html",
+                templateUrl: "templates/item_view.html",
                 controller: 'ItemDetailCtrl'
             })
             .state('mechanism', {
                 url: "/mechanism",
                 templateUrl: "templates/mechanism.html",
                 controller: 'MechanismCtrl'
+            })
+            .state('mechanismDetail', {
+                url: "/mechanism/:mechanismId",
+                templateUrl: "templates/mechanism_view.html",
+                controller: 'MechanismDetailCtrl'
+            })
+            .state('report', {
+                url: "/report",
+                templateUrl: "templates/report.html",
+                controller: 'ReportCtrl'
+            })
+            .state('reportDetail', {
+                url: "/report/:reportId",
+                templateUrl: "templates/report_view.html",
+                controller: 'ReportDetailCtrl'
             });
 
         $urlRouterProvider.otherwise("/hero");
         $httpProvider.defaults.useXDomain = true;
+        $sceProvider.enabled(false);
     });
 
 //todo : 공통으로 사용되는 static 파일 (메뉴) 분리하기
