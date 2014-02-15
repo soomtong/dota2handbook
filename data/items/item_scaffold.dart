16b8a909c806309d4dd335b7f657c59aedfbcc0a
Soomtong-Mac <soomtong@gmail.com>
2014-02-15 23:52:57 +0900

    update item content

--- data/items/item_scaffold.dart
+++ data/items/item_scaffold.dart
@@ -17,19 +17,37 @@ void main () {
   var herosFile = new File(heroesFileName).readAsString().then((String data) {
     List itemsData = JSON.decode(data);
 
-    var itemName, itemTitle, fileName, description, content;
+    var fileName, content;
     var createdFileCount = 0;
 
     for (var item in itemsData) {
-      itemName = item['id'];
-      itemTitle = item['title'] + ' ' + item['subtitle'];
-      description = item['additional'] + item['desc'];
-      fileName = itemName + '.json';
+      fileName = item['id'] + '.json';
       content ='''
 {
-    "id": "${itemName}",
-    "title": "${itemTitle}",
-    "description": "${description}"
+    "id": "${item['id']}",
+    "title": "${item['title'] + ' ' + item['subtitle']}",
+    "desc": "${item['additional']}",
+    "img": "${item['pic']}",
+    "cost": "${item['price']}",
+    "story": "${item['desc']}",
+    "info": "${item['desc']}",
+    "table": [
+        ["마나", "4"],
+        ["쿨다운", "4"]
+    ],
+    "note": [
+        "100MP / 30s",
+        "공격을 받으면 마나 재생이 중단된다.",
+        "1초당 33마나 재생"
+    ],
+    "ability": "지정된 대상(더블 클릭시 자신에게 적용)",
+    "affect": "아군",
+    "type": "마법 또는 일반   ",
+    "recipe": [
+        ["clarity", "data/images/items/clarity_lg.png" , 2],
+        ["bf4ury", "data/images/items/bfury_lg.png" ],
+        ["blink", "data/images/items/blink_lg.png"]
+    ]
 }
 ''';
 
