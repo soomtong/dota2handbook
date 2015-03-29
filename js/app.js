5da378b3a2974b2299b2a9a04fc9493ec5a1e1b5
soomtong <soomtong@gmail.com>
2015-03-29 15:05:17 +0900

    setup react

--- js/app.js
+++ js/app.js
@@ -1,27 +1,24 @@
-//var itemData, heroData, articleData;
+var itemData, heroData, articleData;
 
 // load data entry
 $.getJSON('data/items/items.json', function (data) {
     var dataList = _.sortBy(data, "title");
-
-    riot.mount('item-selector');
-    riot.mount('items', dataList);
+    itemData = dataList;
+    //riot.mount('item-selector');
+    //riot.mount('items', dataList);
 });
 
 $.getJSON('data/heroes/heroes.json', function (data) {
     var dataList = _.sortBy(data, "title");
 
-    riot.mount('heroes', dataList);
+    //riot.mount('heroes', dataList);
 });
 
 $.getJSON('data/article/articles.json', function (data) {
     var dataList = _.sortBy(data, "title");
 
-    riot.mount('articles', dataList);
+    //riot.mount('articles', dataList);
 });
 
 // set page selector
-riot.mount('selector');
-
-// set global
-riot.settings.brackets = '{{ }}';
+//riot.mount('selector');
--- tag/selector.js
+++ tag/selector.js
@@ -1,13 +1,148 @@
-<selector>
-    <div class="selector">
-        <div class="forms-inline-list right">
-            <button class="btn btn-smaller btn-outline btn-round" show={{ opts.name != 'item'}}>아이템</button>
-            <button class="btn btn-smaller btn-outline btn-round" show={{ opts.name != 'hero'}}>영웅</button>
-            <button class="btn btn-smaller btn-outline btn-round" show={{ opts.name != 'article'}}>문서</button>
-        </div>
-    </div>
-    <script>
-        var selected = this.opts.name;
-        console.log(selected);
-    </script>
-</selector>
+var PanelSelector = React.createClass({
+    getInitialState : function(){
+        return {
+            panel: this.props.panel || 'item'
+        }
+    },
+    render: function() {
+        var selector = this.state.panel;
+        var panel = [{id: 'item', title: '아이템'}, {id: 'hero', title: '영웅'}, {id: 'article', title: '문서'}];
+
+        return (
+            <div className="selector">
+                <div className="forms-inline-list right">
+                    {
+                        panel.map(function (i) {
+                            if (i.id != selector) {
+                                return <button className="btn btn-smaller btn-outline btn-round" key={ i.id }>{ i.title }</button>
+                            }
+                        })
+                    }
+                </div>
+            </div>
+        );
+    }
+});
+
+var OrderSelector = React.createClass({
+    render: function () {
+        return (
+            <div className="order">
+                <ul className="forms-inline-list">
+                    <li>
+                        <input type="radio" name="item_list_order" id="radio-item-grade"/>
+                        <label htmlFor="radio-item-grade">등급</label>
+                    </li>
+                    <li>
+                        <input type="radio" name="item_list_order" id="radio-item-name1"/>
+                        <label htmlFor="radio-item-name1">한글 이름</label>
+                    </li>
+                    <li>
+                        <input type="radio" name="item_list_order" id="radio-item-name2"/>
+                        <label htmlFor="radio-item-name2">영문 이름</label>
+                    </li>
+                    <li>
+                        <input type="radio" name="item_list_order" id="radio-item-price"/>
+                        <label htmlFor="radio-item-price">가격</label>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+});
+
+var ItemFilter = React.createClass({
+    render: function () {
+        return (
+            <div className="choice">
+                <ul className="forms-inline-list">
+                    <li>
+                        <input type="checkbox" name="checkbox-1" id="checkbox-1"/>
+                        <label htmlFor="checkbox-1">힘</label>
+                    </li>
+                    <li>
+                        <input type="checkbox" name="checkbox-2" id="checkbox-2"/>
+                        <label htmlFor="checkbox-2">민첩</label>
+                    </li>
+                    <li>
+                        <input type="checkbox" name="checkbox-3" id="checkbox-3"/>
+                        <label htmlFor="checkbox-3">지능</label>
+                    </li>
+                    <li>
+                        <input type="checkbox" name="checkbox-4" id="checkbox-4"/>
+                        <label htmlFor="checkbox-4">모든능력치</label>
+                    </li>
+                    <li>
+                        <input type="checkbox" name="checkbox-5" id="checkbox-5"/>
+                        <label htmlFor="checkbox-5">체력</label>
+                    </li>
+                    <li>
+                        <input type="checkbox" name="checkbox-6" id="checkbox-6"/>
+                        <label htmlFor="checkbox-6">마나</label>
+                    </li>
+                    <br/>
+                    <li>
+                        <input type="checkbox" name="item-shop-side" id="item-shop-side"/>
+                        <label htmlFor="item-shop-side">사이드 상점</label>
+                    </li>
+                    <li>
+                        <input type="checkbox" name="item-shop-secret" id="item-shop-secret"/>
+                        <label htmlFor="item-shop-secret">비밀 상점</label>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+});
+
+var Items = React.createClass({
+    getInitialState : function(){
+        return {
+            dataList: []
+        }
+    },
+    componentDidMount: function () {
+        $.getJSON('data/items/items.json', function (data) {
+            if (this.isMounted()) {
+                console.log('mount');
+                this.setState({ dataList: data });
+            }
+        }.bind(this));
+    },
+    render: function () {
+        return (
+            <div className="nav nav-stats list">
+                <ul>
+                    {
+                        this.state.dataList.map(function (item) {
+                            return (
+                                <li key={ item.id }>
+                                    <img src={ item.pic } alt={ item.subtitle }/>
+                                    <b>{ item.title }</b>
+                                    <small className="subtitle">{ item.subtitle }</small>
+                                    <b className="badge badge-small badge-black">{ Item.showType(item.item_category) }</b>
+                                </li>
+                            );
+                        })
+                    }
+                </ul>
+            </div>
+        );
+    }
+});
+
+var ItemPanel = React.createClass({
+    render: function () {
+        return (
+            <div className="unit-33 items" id="items">
+                <PanelSelector panel="item"/>
+                <h1>아이템</h1>
+                <OrderSelector/>
+                <ItemFilter/>
+                <Items/>
+            </div>
+        );
+    }
+});
+
+React.render(<ItemPanel/>, document.getElementById('wrap'));
