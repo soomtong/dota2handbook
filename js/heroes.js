a344d6ee2603d6be012bcf2abaee7b9547afd563
soomtong <soomtong@gmail.com>
2015-04-05 03:17:15 +0900

    update label

--- js/heroes.js
+++ js/heroes.js
@@ -228,9 +228,9 @@ var heroData = {
         {id: 'speed', title: '이동속도​'},
         {id: 'armor', title: '방어도​'},
         {id: 'range', title: '사거리​'},
-        {id: 'pow_inc', title: '힘 증가'},
-        {id: 'agi_inc', title: '민 증가'},
-        {id: 'int_inc', title: '지 증가'}
+        {id: 'pow_inc', title: '힘 증가량'},
+        {id: 'agi_inc', title: '민첩 증가량'},
+        {id: 'int_inc', title: '지능 증가량'}
     ],
     orderTable: {
         common: 'id',
